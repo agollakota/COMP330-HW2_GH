c99338fd0b3f352e77b6ee25c82cb1c4605e059f
Aparna Gollakota <agollakota@luc.edu>
2017-10-16 22:06:04 +0000

    notebook creator class

--- Project/NotebookCreator.java
+++ Project/NotebookCreator.java
@@ -2,5 +2,39 @@
 
 //in a way - try and combine the topo sort w this?
 
+//Documentation/Source:
 
-//test 
+//https://www.mkyong.com/java/how-to-create-directory-in-java/ -- how to create a new folder
+
+package com.mkyong.file;
+
+import java.io.File;
+
+public class CreateDirectoryExample {
+
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Directory1");
+        if (!file.exists()) {
+            if (file.mkdir()) {
+                System.out.println("Directory is created!");
+            } else {
+                System.out.println("Failed to create directory!");
+            }
+        }
+
+        File files = new File("C:\\Directory2\\Sub2\\Sub-Sub2");
+        if (!files.exists()) {
+            if (files.mkdirs()) {
+                System.out.println("Multiple directories are created!");
+            } else {
+                System.out.println("Failed to create multiple directories!");
+            }
+        }
+
+    }
+
+}
+
+
+
