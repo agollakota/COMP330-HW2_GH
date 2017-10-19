f71de793db2e9d36484883e2c71c2812e5046b63
Chris <celliott1@luc.edu>
2017-10-19 07:53:47 -0500

    Added finished Paint class and accompanying Paint GUI

--- Project/Paint.java
+++ Project/Paint.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -8,90 +6,136 @@ import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
+//Adapted from - http://forum.codecall.net/topic/58137-java-mini-paint-program/
 
-public class Paint extends JComponent {
+@SuppressWarnings("serial")
+class Paint extends JComponent{
+	private int brushSize = 18;
+	private boolean state = true;
+	BufferedImage image;		//Image we draw on
+	Graphics2D graphics2D;		//allows drawing
+	int curX, curY, prevX, prevY;		//mouse coordinates
 
-    private Image image;
 
-    private Graphics2D g2;
-    private int currentX, currentY, oldX, oldY;
+	public Paint(){
+		setDoubleBuffered(false);
+		
+		//adds mouse listener that will set prevX and prevY to coordinates of click
+		addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e){
+				prevX = e.getX();
+				prevY = e.getY();
+			}
+		});
+		
+		//adds mouse motion listener to paint as mousebutton is held down
+		//draws the line at the input coordinates
+		addMouseMotionListener(new MouseMotionAdapter(){
+			public void mouseDragged(MouseEvent e){
+				curX = e.getX();
+				curY = e.getY();
+				
+				if(state == true) {
+				if(graphics2D != null)
+				graphics2D.drawLine(prevX, prevY, curX, curY);
+				repaint();
+				prevX = curX;
+				prevY = curY;
+				
+				} else if (state == false) {
+				graphics2D.fillOval((prevX - (brushSize / 2)), (prevY - (brushSize / 2)), brushSize, brushSize);
+                repaint();
 
-    public Paint() {
-        setDoubleBuffered(false);
-        addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                // save coord x,y when mouse is pressed
-                oldX = e.getX();
-                oldY = e.getY();
-            }
-        });
-
-        addMouseMotionListener(new MouseMotionAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                // coord x,y when drag mouse
-                currentX = e.getX();
-                currentY = e.getY();
-
-                if (g2 != null) {
-
-                    g2.drawLine(oldX, oldY, currentX, currentY);
-
-                    repaint();
-
-                    oldX = currentX;
-                    oldY = currentY;
-                }
-            }
-        });
-    }
-
-    protected void paintComponent(Graphics g) {
-        if (image == null) {
-
-            image = createImage(getSize().width, getSize().height);
-            g2 = (Graphics2D) image.getGraphics();
-
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-            clear();
-        }
-
-        g.drawImage(image, 0, 0, null);
-    }
-
-    public void clear() {
-        g2.setPaint(Color.white);
-
-        g2.fillRect(0, 0, getSize().width, getSize().height);
-        g2.setPaint(Color.black);
-        repaint();
-    }
-
-    public void red() {
-
-        g2.setPaint(Color.red);
-    }
-
-    public void black() {
-        g2.setPaint(Color.black);
-    }
-
-    public void yellow () {
-        g2.setPaint(Color.YELLOW);
-    }
-
-    public void green() {
-        g2.setPaint(Color.green);
-    }
-
-    public void blue() {
-        g2.setPaint(Color.blue);
-    }
-
-    public void white() {
-        g2.setPaint(Color.white);
-    }
+                prevX = curX;
+                prevY = curY;
+			}
+		}
+	});
 }
+
+	//Sets graphic and image
+	public void paintComponent(Graphics g){
+		if(image == null){
+			image = new BufferedImage(getSize().width, getSize().height, BufferedImage.TYPE_3BYTE_BGR);
+			graphics2D = (Graphics2D)image.getGraphics();
+			graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			clear();
+		}
+		g.drawImage(image, 0, 0, null);
+	}
+
+	//sets color to white and fills frame with said color
+	//switches the color back to chosen paint color
+	public void clear(){
+		graphics2D.setPaint(Color.white);
+		graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+		graphics2D.setPaint(Color.black);
+		repaint();
+	}
+
+	//Available colors
+	public void red(){
+		graphics2D.setPaint(Color.red);
+		repaint();
+	}
+	public void black(){
+		graphics2D.setPaint(Color.black);
+		repaint();
+	}
+	public void magenta(){
+		graphics2D.setPaint(Color.magenta);
+		repaint();
+	}
+	public void blue(){
+		graphics2D.setPaint(Color.blue);
+		repaint();
+	}
+	public void green(){
+		graphics2D.setPaint(Color.green);
+		repaint();
+	}
+	public void yellow(){
+		graphics2D.setPaint(Color.yellow);
+		repaint();
+	}
+	public void gray() {
+		graphics2D.setPaint(Color.gray);
+		repaint();
+	}
+	public void pink() {
+		graphics2D.setPaint(Color.pink);
+		repaint();
+	}
+	public void cyan() {
+		graphics2D.setPaint(Color.cyan);
+		repaint();
+	}
+	public void orange() {
+		graphics2D.setPaint(Color.orange);
+		repaint();
+	}
+	public void erase() {
+		graphics2D.setPaint(Color.white);
+		repaint();
+	}
+	public void setState(Boolean val) {
+		this.state = val;
+	}
+	   public void save(File path) throws IOException{
+	        ImageIO.write(image, "PNG", new File(path.getPath().toString() + ".png"));
+	    }
+
+	    public void load(File file) throws IOException {
+	        image = ImageIO.read(new File(file.getPath().toString()));
+	        repaint();
+	    }
+}
+
+
---
+++ Project/PaintGui.java
@@ -0,0 +1,316 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+//Adapted from - http://forum.codecall.net/topic/58137-java-mini-paint-program/
+
+@SuppressWarnings("serial")
+public class PaintGui extends JFrame {
+
+	public PaintGui() {
+
+		super("Paint");
+
+		createView();
+
+		setIconImage(Toolkit.getDefaultToolkit().getImage("octopus.png"));
+
+		setMinimumSize(new Dimension(600, 600));
+
+		pack();
+
+		setLocationRelativeTo(null);
+
+		setResizable(false);
+	}
+
+	public void createView() {
+
+		// instantiate Paint class to call methods
+		final Paint paint = new Paint();
+		Container content = getContentPane();
+		content.setLayout(new BorderLayout());
+
+		content.add(paint, BorderLayout.CENTER);
+
+		// Setting JPanel details
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(32, 68));
+		panel.setMinimumSize(new Dimension(32, 68));
+		panel.setMaximumSize(new Dimension(32, 68));
+
+		//Setting JMenuBar Details + actionListeners for MenuItems--> Open/Save files
+		JMenuBar menu = new JMenuBar();
+		setJMenuBar(menu);
+		menu.setOpaque(true);
+		
+		JMenu menuFile = new JMenu("File");
+		
+		menu.add(menuFile);
+		
+		JMenuItem open = new JMenuItem("Open File");
+		menuFile.add(open);
+		
+		JFileChooser fc = new JFileChooser();
+		
+		open.addActionListener(ev -> {
+			 int returnVal = fc.showOpenDialog(panel);
+		      if (returnVal == JFileChooser.APPROVE_OPTION) {
+		        File file = fc.getSelectedFile();
+		        try {
+		        	paint.load(file);
+		        } catch (Exception e) {
+		          e.printStackTrace();
+		        }
+		      } else {
+		        System.out.println("Operation Cancelled...");
+		      }
+		});
+		
+		JMenuItem save = new JMenuItem("Save File");
+		menuFile.add(save);
+		
+		save.addActionListener(ev -> {
+			int returnVal = fc.showOpenDialog(panel);
+		      if (returnVal == JFileChooser.APPROVE_OPTION) {
+		    	  File file = fc.getSelectedFile();
+		        try {
+		           paint.save(file);
+		        } catch (Exception e) {
+		          e.printStackTrace();
+		        }
+		      } else {
+		        System.out.println("Operation Cancelled...");
+		      }
+		});
+		// Setting buttons for changing colors
+		JButton redButton = new JButton();
+		redButton.setBackground(Color.red);
+		redButton.setToolTipText("Red");
+		redButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.red();
+			}
+
+		});
+
+		JButton blackButton = new JButton();
+		blackButton.setToolTipText("Black");
+		blackButton.setBackground(Color.black);
+		blackButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.black();
+			}
+		});
+
+		JButton magentaButton = new JButton();
+		magentaButton.setToolTipText("Magenta");
+		magentaButton.setBackground(Color.magenta);
+		magentaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.magenta();
+			}
+		});
+
+		JButton blueButton = new JButton();
+		blueButton.setBackground(Color.blue);
+		blueButton.setToolTipText("Blue");
+		blueButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.blue();
+			}
+		});
+
+		JButton greenButton = new JButton();
+		greenButton.setBackground(Color.green);
+		greenButton.setToolTipText("Green");
+		greenButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.green();
+			}
+		});
+
+		JButton yellowButton = new JButton();
+		yellowButton.setBackground(Color.yellow);
+		yellowButton.setToolTipText("Yellow");
+		yellowButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.yellow();
+			}
+		});
+
+		JButton grayButton = new JButton();
+		grayButton.setBackground(Color.gray);
+		grayButton.setToolTipText("Gray");
+		grayButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.gray();
+			}
+		});
+
+		JButton pinkButton = new JButton();
+		pinkButton.setBackground(Color.pink);
+		pinkButton.setToolTipText("Pink");
+		pinkButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.pink();
+			}
+		});
+
+		JButton cyanButton = new JButton();
+		cyanButton.setBackground(Color.cyan);
+		cyanButton.setToolTipText("Cyan");
+		cyanButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.cyan();
+			}
+		});
+
+		JButton orangeButton = new JButton();
+		orangeButton.setBackground(Color.orange);
+		orangeButton.setToolTipText("Orange");
+		orangeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.orange();
+			}
+		});
+
+		// Setting png file as background for eraserButton
+		ImageIcon tempIcon = new ImageIcon(Toolkit.getDefaultToolkit().getImage("erasericon.png"));
+		Image img = tempIcon.getImage();
+		// resizes erasericon.png
+		Image newimg = img.getScaledInstance(16, 16, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon eraseIcon = new ImageIcon(newimg);
+
+		// get rid of border and content area in order to use erasericon.png as button
+		JButton eraseButton = new JButton(new ImageIcon(eraseIcon.getImage()));
+		eraseButton.setToolTipText("Click to use eraser.");
+		eraseButton.setBorder(BorderFactory.createEmptyBorder());
+		eraseButton.setContentAreaFilled(false);
+
+		eraseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.erase();
+			}
+		});
+
+		// Setting clear button for clearing screen of paint
+		// Setting png file as background for clearButton
+		ImageIcon fillIcon = new ImageIcon(Toolkit.getDefaultToolkit().getImage("clearbutton.png"));
+		Image tempImg = fillIcon.getImage();
+
+		// resizes clearbutton.png
+		Image clearImg = tempImg.getScaledInstance(16, 16, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon clearIcon = new ImageIcon(clearImg);
+
+		// get rid of border and content area in order to use clearbutton.png as button
+		JButton clearButton = new JButton(new ImageIcon(clearIcon.getImage()));
+		clearButton.setToolTipText("Click to clear canvas.");
+		clearButton.setBorder(BorderFactory.createEmptyBorder());
+		clearButton.setContentAreaFilled(false);
+
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.clear();
+			}
+		});
+		
+		// Setting smallerbrush button for clearing screen of paint
+		// Setting png file as background for smallButton
+		ImageIcon filler = new ImageIcon(Toolkit.getDefaultToolkit().getImage("smallerbrush.png"));
+		Image holdImg = filler.getImage();
+
+		// resizes smallerbrush.png
+		Image smallImg = holdImg.getScaledInstance(16, 16, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon smallIcon = new ImageIcon(smallImg);
+
+		// get rid of border and content area in order to use smallerbrush.png as button
+		JButton smallButton = new JButton(new ImageIcon(smallIcon.getImage()));
+		smallButton.setToolTipText("Click to make toolsize small.");
+		smallButton.setBorder(BorderFactory.createEmptyBorder());
+		smallButton.setContentAreaFilled(false);
+
+		smallButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.setState(true);
+			}
+		});
+
+		smallButton.setBorder(BorderFactory.createEmptyBorder());
+		smallButton.setContentAreaFilled(false);
+		smallButton.setFont(smallButton.getFont().deriveFont(Font.BOLD));
+
+		// Setting clear button for clearing screen of paint
+		// Setting png file as background for bigButton
+		ImageIcon bigIcon = new ImageIcon(Toolkit.getDefaultToolkit().getImage("biggerbrush.png"));
+		Image bigImg = bigIcon.getImage();
+
+		// resizes biggerbrush.png
+		Image biggerImg = bigImg.getScaledInstance(16, 16, java.awt.Image.SCALE_SMOOTH);
+		ImageIcon biggerIcon = new ImageIcon(biggerImg);
+
+		// get rid of border and content area in order to use biggerbrush.png as button
+		JButton bigButton = new JButton(new ImageIcon(biggerIcon.getImage()));
+		bigButton.setToolTipText("Click to make toolsize big.");
+		bigButton.setBorder(BorderFactory.createEmptyBorder());
+		bigButton.setContentAreaFilled(false);
+
+		bigButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				paint.setState(false);
+			}
+		});
+
+		// Set button sizes
+		blackButton.setPreferredSize(new Dimension(16, 16));
+		magentaButton.setPreferredSize(new Dimension(16, 16));
+		redButton.setPreferredSize(new Dimension(16, 16));
+		blueButton.setPreferredSize(new Dimension(16, 16));
+		greenButton.setPreferredSize(new Dimension(16, 16));
+		yellowButton.setPreferredSize(new Dimension(16, 16));
+		grayButton.setPreferredSize(new Dimension(16, 16));
+		pinkButton.setPreferredSize(new Dimension(16, 16));
+		cyanButton.setPreferredSize(new Dimension(16, 16));
+		orangeButton.setPreferredSize(new Dimension(16, 16));
+
+		// Add to buttonPanel
+		panel.add(greenButton);
+		panel.add(blueButton);
+		panel.add(magentaButton);
+		panel.add(blackButton);
+		panel.add(redButton);
+		panel.add(yellowButton);
+		panel.add(grayButton);
+		panel.add(pinkButton);
+		panel.add(cyanButton);
+		panel.add(orangeButton);
+		panel.add(smallButton);
+		panel.add(bigButton);
+		panel.add(eraseButton);
+		panel.add(clearButton);
+
+		// Add panel to left side of GUI
+		content.add(panel, BorderLayout.WEST);
+	}
+}
